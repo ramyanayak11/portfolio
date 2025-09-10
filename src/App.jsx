fb011b4fe98cac04bc82a048fcb872f9d587c8e7
Ramya Nayak <153323743+ramyanayak11@users.noreply.github.com>
2025-09-10 12:44:36 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <Sidebar />
       <main>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/portfolio/" element={<Home/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/work" element={<Work/>} />
           <Route path="/connect" element={<Connect/>} />
