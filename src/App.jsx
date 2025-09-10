4c243e198780043c48acc601247c74985ade0fdf
Ramya Nayak <153323743+ramyanayak11@users.noreply.github.com>
2025-09-10 12:56:18 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,16 +7,18 @@ import Connect from "./pages/Connect";
 
 export default function App() {
   return (
-    <div className="app">
-      <Sidebar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/work" element={<Work/>} />
-          <Route path="/connect" element={<Connect/>} />
-        </Routes>
-      </main>
-    </div>
+    <BrowserRouter basename="/portfolio">
+      <div className="app">
+        <Sidebar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/connect" element={<Connect />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
